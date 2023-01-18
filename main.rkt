11d4ec9d4641fa7568646243b46c932ac445e04a
Yasuhiro Яша Asaka <yasuhiro.asaka@grauwoelfchen.net>
2023-01-19 05:31:59 +0900

    Add book and update struct definitions

--- main.rkt
+++ main.rkt
@@ -10,6 +10,24 @@
         (date->string (seconds->date (current-seconds))))
 |#
 
+(struct update (read_up_to updated) #:transparent)
+(struct book (title isbn total_page_numbers update) #:transparent)
+
+;; creates a book. this takes a hasheq object represents book entry.
+;; a book struct like looks like:
+;;   (book
+;;     "title" "isbn" total_page_numbers
+;;     (update read_up_to #<datetime updated_at>))
+(define (create-book br)
+  (book (hash-ref br 'title)
+        (hash-ref br 'isbn)
+        (hash-ref br 'total_page_numbers)
+        (if (hash-has-key? br 'updates)
+          (let ([lu (first (hash-ref br 'updates))])
+            (update (hash-ref lu 'read_up_to)
+                    (hash-ref lu 'updated_at)))
+          empty)))
+
 ;; reads files in the data directory and generates a list contains all books
 (define (read-data path)
   ;; returns readable file or not
@@ -17,21 +35,10 @@
     (member 'read (file-or-directory-permissions f)))
 
   (for/list ([f (in-directory path user-readable?)])
-    ;; hasheq
+    ;; parse-toml creates a hasheq object
     (let* ([ht (parse-toml (port->string (open-input-file f) #:close? #t))]
-           [book (hash-ref ht 'book)])
-      ;; format:
-      ;;   '("title" "isbn" total_page_numbers
-      ;;     (read_up_to #<datetime updated_at>))
-      (list
-        (hash-ref book 'title)
-        (hash-ref book 'isbn)
-        (hash-ref book 'total_page_numbers)
-        (if (hash-has-key? book 'updates)
-          (list
-            (hash-ref (first (hash-ref book 'updates)) 'read_up_to)
-            (hash-ref (first (hash-ref book 'updates)) 'updated_at))
-          '())))))
+           [br (hash-ref ht 'book)])
+      (create-book br))))
 
 ;; TODO: create progress `bar`s
 (for-each pretty-print (read-data "./data"))
