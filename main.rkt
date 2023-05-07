9c0d1aafb1a04695079ddfbdda327f57fcabc623
Yasuhiro Яша Asaka <yasuhiro.asaka@grauwoelfchen.net>
2023-05-08 02:14:53 +0900

    Update bar to print progress bars XD

--- main.rkt
+++ main.rkt
@@ -1,12 +1,33 @@
 #lang racket/base
 
+(require racket/format)
+(require racket/list)
+(require racket/math)
 (require racket/pretty)
 
 (require "data.rkt")
 
-;; TODO: create progress `bar`s
-;; ~~prints all reading list data~~ returns only `is_focused` for now
-(for-each pretty-print
+(define bar-width 50)
+
+(for-each (lambda (b)
+            ;; skip "random" reading order books (for now)
+            (if (and (equal? (approach-order (book-reading_approach b))
+                             "sequential")
+                     (not (empty? (book-update b))))
+              (let* ([p (an-update-seq-read_up_to (book-update b))]
+                     [v (* (/ p (book-total_page_numbers b)) 100)]
+                     [s (exact-round (* (/ bar-width 100) v))])
+                ;; prints a progress bar
+                (displayln (format
+                             "~a:\nReading [~a~a] ~a/~a ~a%\n"
+                             (book-title b)
+                             (make-string s #\=)
+                             (make-string (- bar-width s) #\-)
+                             p
+                             (book-total_page_numbers b)
+                             (~r v #:precision 2))))
+              0))
           (filter
-            (lambda (d) (status-is_focused (book-status d)))
+            ;; prints only `is_focused` entries by default
+            (lambda (b) (status-is_focused (book-status b)))
             (read-data "./data")))
